b5046b22a933f0bc075889908d95496724d66be3
NicholasK <nicholask1@outlook.com>
2021-11-11 10:29:13 -0500

    Inital Commit

---
+++ app.js
@@ -0,0 +1,22 @@
+// Question 1
+var PI = 3.14
+function question() {
+    PI = 42;
+    return PI;
+}
+// Question 2
+const PI = 3.14
+
+// QUIZ
+
+// 1
+// var and let have the same functionality execpt that let does not allow you to redeclare varaible and let uses block scope instead of function scope
+
+// 2
+// Const does not allow for a variable to reassigned or redeclared like var does
+
+// 3
+// const and let have the same differenaces as const has with var, but const and let both use block scope and not function scope
+
+// 4
+//  Varibale hosting is when variable declarations in moved to the top of the file. 
